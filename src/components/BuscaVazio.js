632ab1d47ea0bc06f07e1bce5f37a47ac1e9a4e6
Andre Lucas <andrelucca99@gmail.com>
2022-05-24 18:23:55 -0300

    requisito 5

---
+++ src/components/BuscaVazio.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class BuascaVazio extends React.Component {
+    render() {
+        return (
+            <p>"Nenhum produto foi encontrado"</p>
+        );
+    }
+}
+
+export default BuascaVazio;
--- src/components/Listagem.js
+++ src/components/Listagem.js
@@ -1,13 +1,45 @@
 import React from 'react';
+import {getCategories} from '../services/api'
+import BuascaVazio from './BuscaVazio';
 
 class Listagem extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      buscar: '',
+    }
+  }
+
+  handleBusca = async () => {
+    const { buscar } = this.state;
+    await getCategories(buscar)
+  }
+
   render() {
+    const { buscar } = this.state;
     return (
       <>
-        <input type="text" placeholder="busca" />
+        <input
+        data-testid="query-input"
+        type="text"
+        placeholder="busca"
+        onChange={ (({target}) => {
+          this.setState({ buscar: target.value })
+        }) } />
+
+        <button data-testid="query-button" type="button" onClick={ this.handleBusca }>
+        
+          Pesquisar</button>
+
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
+
+        { !buscar && (
+          <BuascaVazio />
+        ) }
+
       </>
     );
   }
