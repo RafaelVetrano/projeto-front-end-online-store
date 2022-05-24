785b39c8a0a8058bb46a14d91cc1ee8a1dea9a5a
Felipe <lionffl@gmail.com>
2022-05-24 18:27:25 -0300

    task 4 ongoing

--- src/components/Listagem.js
+++ src/components/Listagem.js
@@ -1,13 +1,31 @@
 import React from 'react';
+import { getCategories } from '../services/api';
 
 class Listagem extends React.Component {
+state = {
+  categorias: [],
+}
+
+  componentDidMount = async () => {
+    const response = await getCategories();
+    this.setState({ categorias: response });
+  }
+
   render() {
+    const { categorias } = this.state;
+
     return (
       <>
         <input type="text" placeholder="busca" />
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
+
+        { categorias.map(({ name, id }) => (
+          <>
+            <input type="radio" key={ id } data-testid="category" id="category" />
+            <label htmlFor="category">{name}</label>
+          </>))}
       </>
     );
   }
--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,11 @@
 export async function getCategories() {
-  const response = await (await fetch('https://api.mercadolibre.com/sites/MLB/categories')).json();
-  return response;
+  const response = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  const objJSON = response.json();
+  return objJSON;
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  const response = await (await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`)).json();
-  return response;
+  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
+  const objJSON = response.json();
+  return objJSON;
 }
