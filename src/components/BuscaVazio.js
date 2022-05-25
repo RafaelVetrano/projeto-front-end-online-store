25ba601213fcc1513b3b5d93d2f4fec01da3f8ad
viniciuscardoso-dev <cardosovinicius87@gmail.com>
2022-05-25 16:25:57 -0300

    requisito 3, 4 e 5

---
+++ src/components/BuscaVazio.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class BuascaVazio extends React.Component {
+  render() {
+    return <p>Nenhum produto foi encontrado</p>;
+  }
+}
+
+export default BuascaVazio;
--- src/components/Listagem.js
+++ src/components/Listagem.js
@@ -1,19 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { getCategories } from '../services/api';
+import { getCategories, getProductsFromCategoryAndQuery } from '../services/api';
+import BuascaVazio from './BuscaVazio';
 
 class Listagem extends React.Component {
-state = {
-  categorias: [],
-}
+  constructor() {
+    super();
+
+    this.state = {
+      buscar: '',
+      produto: [],
+      categorias: [],
+    };
+    this.handleBusca = this.handleBusca.bind(this);
+  }
 
   componentDidMount = async () => {
     const response = await getCategories();
     this.setState({ categorias: response });
   }
 
+  handleBusca = async () => {
+    const { buscar } = this.state;
+    const produtos = await getProductsFromCategoryAndQuery(buscar);
+    this.setState({ produto: produtos.results });
+  }
+
   render() {
-    const { categorias } = this.state;
+    const { categorias, produto } = this.state;
 
     return (
       <>
@@ -21,6 +35,14 @@ state = {
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
+        <input
+          data-testid="query-input"
+          type="text"
+          placeholder="busca"
+          onChange={ ({ target }) => {
+            this.setState({ buscar: target.value });
+          } }
+        />
         <Link
           to="/cart"
           data-testid="shopping-cart-button"
@@ -30,11 +52,30 @@ state = {
           </span>
         </Link>
 
+        <button
+          data-testid="query-button"
+          type="button"
+          onClick={ this.handleBusca }
+        >
+          Pesquisar
+        </button>
+
         { categorias.map(({ name, id }) => (
           <div key={ id }>
             <input type="radio" data-testid="category" id={ id } name="category" />
             <label htmlFor={ id }>{name}</label>
           </div>))}
+        {!produto ? (
+          <BuascaVazio />
+        ) : (
+          produto.map(({ thumbnail, title, price, id }) => (
+            <div data-testid="product" key={ id }>
+              <p>{ title }</p>
+              <img src={ thumbnail } alt={ title } />
+              <p>{ price }</p>
+            </div>
+          ))
+        )}
       </>
     );
   }
